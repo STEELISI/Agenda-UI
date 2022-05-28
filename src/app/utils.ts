de624ccdfcb9388f4bb197a05c13861398515e01
pithayuth.c <charnset@usc.edu>
2022-05-28 02:38:20 -0700

    update utils.ts: modify generate_empty_training_dir.sh to generate directory for nli_premises

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -12,42 +12,59 @@ export function deleteArrayElement(arr: any[], index: number) {
 }
 
 export function generateEmptyTrainingDirSh(agenda_name: string, triggers: string) {
-  let sh_script: string = `#!/bin/bash
+  let sh_script: string = 
+`#!/bin/bash
 if [ "$1" == "" ]; then
-    echo "Need one input argument: path to training data directory"
-    exit
+  echo "Need one input argument: path to training data directory"
+  exit
 fi
 
 echo "PATH: $1";
 echo "AGENDA: ${agenda_name}";
 
-agenda_path=$1/${agenda_name};
+nli_path=$1/nli_premises/${agenda_name};
+nlu_path=$1/nlu_training_data/${agenda_name};
 
-echo "AGENDA_PATH: $agenda_path"
+echo "NLI_PATH: $nli_path"
+echo "NLU_PATH: $nlu_path"
 
-if [ -d "$agenda_path" ]; then
-\techo "$agenda_path directory already existed";
+if [ -d "$nli_path" ]; then
+  echo "$nli_path directory already existed";
 else
-\tmkdir -p $agenda_path;
-\techo "$agenda_path directory is created";
+  mkdir -p $nli_path;
+  echo "$nli_path directory is created";
+fi
+
+if [ -d "$nlu_path" ]; then
+  echo "$nlu_path directory already existed";
+else
+  mkdir -p $nlu_path;
+  echo "$nlu_path directory is created";
 fi
 
 triggers="${triggers}"
 
 for t in $triggers;
 do
-\ttrigger_path=$agenda_path/$t
-\tif [ -d "$trigger_path" ]; then
-\t\techo "$trigger_path directory already existed";
-\telse
-\t\tmkdir -p $trigger_path
-\t\techo "$trigger_path directory is created";
-\t\tcd $trigger_path;
-\t\ttouch $t.txt;
-\t\techo "$t.txt file is created";
-\t\ttouch NOT$t.txt;
-\t\techo "NOT$t.txt file is created";
-\tfi
+  nli_trigger_file=$nli_path/$t.txt
+  if [ -f "$nli_trigger_file" ]; then
+    echo "$nli_trigger_file already existed";
+  else
+    touch $nli_trigger_file;
+    echo "$nli_trigger_file is created";
+  fi
+
+  nlu_trigger_dir=$nlu_path/$t
+  if [ -d "$nlu_trigger_dir" ]; then
+    echo "$nlu_trigger_dir already existed";
+  else
+    mkdir -p $nlu_trigger_dir
+    echo "$nlu_trigger_dir is created";
+    touch $nlu_trigger_dir/$t.txt;
+    echo "$nlu_trigger_dir/$t.txt is created";
+    touch $nlu_trigger_dir/NOT$t.txt;
+    echo "$nlu_trigger_dir/NOT$t.txt is created";
+  fi
 done`;
 
   return sh_script;
