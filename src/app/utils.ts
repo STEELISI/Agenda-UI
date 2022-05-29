c8ccbf1ad8054b987f220fbbba9869ee2008050d
Pithayuth Charnsethikul <pcharnset@piranha-agenda.isi.edu>
2022-05-29 18:51:02 -0400

    update utils.ts: modify generate_empty_training_dir.sh to copy an agenda yaml to the defined agenda dir

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -22,6 +22,17 @@ fi
 echo "PATH: $1";
 echo "AGENDA: ${agenda_name}";
 
+agenda_path=$1/agendas/
+if [ -d "$agenda_path" ]; then
+  echo "$agenda_path directory already existed";
+else
+  mkdir -p $agenda_path;
+  echo "$agenda_path directory is created";
+fi
+
+yaml_file=${agenda_name}.yaml
+cp $yaml_file $agenda_path
+
 nli_path=$1/nli_premises/${agenda_name};
 nlu_path=$1/nlu_training_data/${agenda_name};
 
@@ -39,7 +50,7 @@ if [ -d "$nlu_path" ]; then
   echo "$nlu_path directory already existed";
 else
   mkdir -p $nlu_path;
-  echo "$nlu_path directory is created";
+  echo "$nlu_path directory directory is created";
 fi
 
 triggers="${triggers}"
@@ -48,22 +59,22 @@ for t in $triggers;
 do
   nli_trigger_file=$nli_path/$t.txt
   if [ -f "$nli_trigger_file" ]; then
-    echo "$nli_trigger_file already existed";
+    echo "$nli_trigger_file file already existed";
   else
     touch $nli_trigger_file;
-    echo "$nli_trigger_file is created";
+    echo "$nli_trigger_file file is created";
   fi
 
   nlu_trigger_dir=$nlu_path/$t
   if [ -d "$nlu_trigger_dir" ]; then
-    echo "$nlu_trigger_dir already existed";
+    echo "$nlu_trigger_dir directory already existed";
   else
     mkdir -p $nlu_trigger_dir
-    echo "$nlu_trigger_dir is created";
+    echo "$nlu_trigger_dir file is created";
     touch $nlu_trigger_dir/$t.txt;
-    echo "$nlu_trigger_dir/$t.txt is created";
+    echo "$nlu_trigger_dir/$t.txt file is created";
     touch $nlu_trigger_dir/NOT$t.txt;
-    echo "$nlu_trigger_dir/NOT$t.txt is created";
+    echo "$nlu_trigger_dir/NOT$t.txt file is created";
   fi
 done`;
 
